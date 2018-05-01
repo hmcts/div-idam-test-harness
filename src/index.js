f59c03bfb3b39bde0c1f93c740262723ecb4108b
Tony Chow <tony.chow@hmcts.net>
2018-05-01 09:54:07 +0100

    Accept proxy options for the IDAM test harness as required

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const sinon = require('sinon');
 const request = require('request-promise-native');
 const idamExpress = require('@hmcts/div-idam-express-middleware');
+const socksAgent = require('socks5-https-client/lib/Agent');
+const url = require('url');
+
+const DEFAULT_PORT = 9000;
 
 const onAuthenticate = () => {
   return (req, res, next) => {
@@ -32,7 +36,26 @@ const restore = () => {
   idamExpress.protect.restore();
 };
 
-const createUser = args => {
+const setupProxy = proxy => {
+  const proxyUrl = url.parse(proxy);
+
+  let proxyOptions = {};
+
+  if (proxyUrl.protocol.indexOf('socks') >= 0) {
+    proxyOptions = {
+      strictSSL: false,
+      agentClass: socksAgent,
+      socksHost: proxyUrl.hostname || 'localhost',
+      socksPort: proxyUrl.port || DEFAULT_PORT
+    };
+  } else {
+    proxyOptions = { proxy: proxyUrl.href };
+  }
+
+  return proxyOptions;
+};
+
+const createUser = (args, proxy) => {
   const endpoint = args.accountsEndpoint || '/testing-support/accounts';
   const userGroup = args.testGroupCode || 'divorce-private-beta';
   // Minimum required fields
@@ -44,19 +67,29 @@ const createUser = args => {
     password: args.testPassword || 'passWord123!',
     levelOfAccess: args.testLevelOfAccess || '1'
   };
+
   const options = {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(user)
   };
 
+  if (proxy) {
+    Object.assign(options, setupProxy(proxy));
+  }
+
   return request.post(args.idamApiUrl + endpoint, options);
 };
 
-const removeUser = args => {
+const removeUser = (args, proxy) => {
   const endpoint = args.accountsEndpoint || '/testing-support/accounts';
   const email = args.testEmail || 'testUser@example.com';
 
-  return request.delete(`${args.idamApiUrl + endpoint}/${email}`);
+  let options = {};
+  if (proxy) {
+    options = setupProxy(proxy);
+  }
+
+  return request.delete(`${args.idamApiUrl + endpoint}/${email}`, options);
 };
 
 module.exports = {
