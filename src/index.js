de8e89213ab1b8768f10535468bc714821c7dbc3
Hosain <hosainnet@users.noreply.github.com>
2018-11-15 09:43:49 +0000

    Added authorise/getToken functions (#6)
    * Added authorise/getToken functions
    
    To allow FE apps to have access to the user token for backend calls
    
    * Added support for generating pin
    
    * DIV-3489 - add argument for roles in create user
    
    * Fixing failing tests
    
    * Stop depending on Jenkins pipeline to get PR checks run.
    Doing sonar check in Travis instead.
    
    * DIV-3489 - set version number to 1.3.0

--- src/index.js
+++ src/index.js
@@ -65,7 +65,8 @@ const createUser = (args, proxy) => {
     surname: args.testSurname || 'User',
     userGroup: { code: userGroup },
     password: args.testPassword || 'passWord123!',
-    levelOfAccess: args.testLevelOfAccess || '1'
+    levelOfAccess: args.testLevelOfAccess || '1',
+    roles: args.roles || []
   };
 
   const options = {
@@ -92,6 +93,85 @@ const removeUser = (args, proxy) => {
   return request.delete(`${args.idamApiUrl + endpoint}/${email}`, options);
 };
 
+const argsToUrlParams = args => {
+  return Object.entries(args)
+    .map(param => {
+      return param.join('=');
+    })
+    .join('&');
+};
+
+const authorize = (args, proxy) => {
+  const params = argsToUrlParams({
+    response_type: 'code',
+    client_id: args.idamClientID,
+    redirect_uri: args.redirectUri
+  });
+  const endpoint = args.authorizeEndpoint || '/oauth2/authorize';
+
+  const email = args.testEmail || 'testUser@example.com';
+  const password = args.testPassword;
+
+  const authHeader = Buffer.from(`${email}:${password}`)
+    .toString('base64');
+
+  const options = {
+    headers: { Authorization: `Basic ${authHeader}` },
+    json: true
+  };
+
+  if (proxy) {
+    Object.assign(options, setupProxy(proxy));
+  }
+  return request.post(`${args.idamApiUrl + endpoint}?${params}`, options);
+};
+
+const getToken = (args, proxy) => {
+  return authorize(args, proxy)
+    .then(response => {
+      const code = response.code;
+      const params = argsToUrlParams({
+        code,
+        grant_type: 'authorization_code',
+        client_id: args.idamClientID,
+        redirect_uri: args.redirectUri,
+        client_secret: args.idamSecret
+      });
+      const endpoint = args.tokenEndpoint || '/oauth2/token';
+
+      const options = {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        json: true
+      };
+      if (proxy) {
+        Object.assign(options, setupProxy(proxy));
+      }
+      return request.post(`${args.idamApiUrl + endpoint}?${params}`, options);
+    });
+};
+
+const generatePin = (args, proxy) => {
+  const requestBody = {
+    firstName: args.firstName || 'first-name',
+    lastName: args.lastName || 'last-name',
+    roles: args.roles || []
+  };
+  const endpoint = args.generatePinEndpoint || '/pin';
+
+  const options = {
+    headers: {
+      Authorization: `Bearer ${args.accessToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: requestBody,
+    json: true
+  };
+  if (proxy) {
+    Object.assign(options, setupProxy(proxy));
+  }
+  return request.post(args.idamApiUrl + endpoint, options);
+};
+
 module.exports = {
   onAuthenticate,
   onLanding,
@@ -99,5 +179,7 @@ module.exports = {
   stub,
   restore,
   createUser,
-  removeUser
+  removeUser,
+  getToken,
+  generatePin
 };
--- src/index.test.js
+++ src/index.test.js
@@ -126,7 +126,7 @@ describe('test harness unit tests', () => {
 
       const expectedOptions = {
         headers: { 'Content-Type': 'application/json' },
-        body: '{"email":"testUser@example.com","forename":"Test","surname":"User","userGroup":{"code":"test"},"password":"passWord123!","levelOfAccess":"1"}',
+        body: '{"email":"testUser@example.com","forename":"Test","surname":"User","userGroup":{"code":"test"},"password":"passWord123!","levelOfAccess":"1","roles":[]}',
         strictSSL: false,
         agentClass: socksAgent,
         socksHost: 'proxyhost',
@@ -143,12 +143,12 @@ describe('test harness unit tests', () => {
     });
 
     it('uses the http proxy when creating a user with a http proxy set', () => {
-      const extendedArgs = Object.assign({}, args, { testGroupCode: 'test' });
+      const extendedArgs = Object.assign({}, args, { testGroupCode: 'test', roles: ['role1', 'role2'] });
       const proxy = 'http://proxyhost:8080';
 
       const expectedOptions = {
         headers: { 'Content-Type': 'application/json' },
-        body: '{"email":"testUser@example.com","forename":"Test","surname":"User","userGroup":{"code":"test"},"password":"passWord123!","levelOfAccess":"1"}',
+        body: '{"email":"testUser@example.com","forename":"Test","surname":"User","userGroup":{"code":"test"},"password":"passWord123!","levelOfAccess":"1","roles":["role1","role2"]}',
         proxy: 'http://proxyhost:8080/'
       };
 
@@ -201,6 +201,121 @@ describe('test harness unit tests', () => {
         testHarness.removeUser(extendedArgs);
       }).to.not.throw();
       expect(request.delete.calledWith(`${idamApiUrl}/accounts/mail@example.com`)).to.equal(true);
+    });
+  });
+
+  describe('#getToken', () => {
+    beforeEach(() => {
+      args = {
+        idamApiUrl,
+        idamClientID: 'some-client-id',
+        idamSecret: 'some-secret',
+        redirectUri: 'some-redirect-uri'
+      };
+      postStub = sinon.stub(request, 'post');
+    });
+
+    afterEach(() => {
+      postStub.restore();
+    });
+
+    it('does not return an error when getting a token', done => {
+      postStub.onFirstCall()
+        .resolves({ code: '123' });
+      postStub.onSecondCall()
+        .returns('some-token-response');
+
+      testHarness.getToken(args)
+        .then(response => {
+          expect(request.post.calledWith(`${idamApiUrl}/oauth2/authorize?response_type=code&client_id=some-client-id&redirect_uri=some-redirect-uri`))
+            .to.equal(true);
+          expect(request.post.calledWith(`${idamApiUrl}/oauth2/token?code=123&grant_type=authorization_code&client_id=some-client-id&redirect_uri=some-redirect-uri&client_secret=some-secret`))
+            .to.equal(true);
+          expect(response)
+            .to.equal('some-token-response');
+          done();
+        })
+        .catch(error => {
+          done(error);
+        });
+    });
+
+    it('returns an error when request fails', () => {
+      postStub.throws();
+
+      expect(() => {
+        testHarness.getToken(args);
+      }).to.throw();
+    });
+
+    it('makes the post call using the passed in params', done => {
+      const extendedArgs = Object.assign({}, args, {
+        authorizeEndpoint: '/auth-endpoint',
+        tokenEndpoint: '/token-endpoint'
+      });
+
+      postStub.onFirstCall()
+        .resolves({ code: '123' });
+      postStub.onSecondCall()
+        .returns('some-token-response');
+
+      testHarness.getToken(extendedArgs)
+        .then(response => {
+          expect(request.post.calledWith(`${idamApiUrl}/auth-endpoint?response_type=code&client_id=some-client-id&redirect_uri=some-redirect-uri`))
+            .to.equal(true);
+          expect(request.post.calledWith(`${idamApiUrl}/token-endpoint?code=123&grant_type=authorization_code&client_id=some-client-id&redirect_uri=some-redirect-uri&client_secret=some-secret`))
+            .to.equal(true);
+          expect(response)
+            .to.equal('some-token-response');
+          done();
+        })
+        .catch(error => {
+          done(error);
+        });
+    });
+  });
+
+  describe('#generatePin', () => {
+    beforeEach(() => {
+      args = { idamApiUrl };
+      postStub = sinon.stub(request, 'post');
+    });
+
+    afterEach(() => {
+      postStub.restore();
+    });
+
+    it('does not return an error when generating a pin', () => {
+      postStub.returns('success');
+
+      expect(() => {
+        testHarness.generatePin(args);
+      }).to.not.throw();
+      expect(request.post.calledWith(`${idamApiUrl}/pin`)).to.equal(true);
+    });
+
+    it('returns an error when trying to generate throws an error', () => {
+      postStub.throws();
+
+      expect(() => {
+        testHarness.generatePin(args);
+      }).to.throw();
+    });
+
+    it('makes the post call using the passed in params', () => {
+      const extendedArgs = Object.assign({}, args, {
+        generatePinEndpoint: '/some-pin-endpoint',
+        firstName: 'some-first-name',
+        lastName: 'some-last-name',
+        roles: ['123']
+      });
+
+      postStub.returns('success');
+
+      expect(() => {
+        testHarness.generatePin(extendedArgs);
+      }).to.not.throw();
+      expect(request.post.calledWith(`${idamApiUrl}/some-pin-endpoint`)).to.equal(true);
     });
   });
 });
